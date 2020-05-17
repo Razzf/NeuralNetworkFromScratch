51659e927bf9b8a5ee8c019f54c3e47610340934
felix <felixsanchez0124@gmail.com>
2020-05-16 19:29:46 -0500

    3 output layer

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-_inputs = np.array([1,2,3])
-weights = np.array([.5, -.4, .3])
-bias = 2
+_inputs = np.array([1, 2, 3, 2.5])
 
-output = _inputs[0] * weights[0] + _inputs[1] * weights[1] + _inputs[2] * weights[2] + bias
+
+weights1 = np.array([.2, .8, -.5, 1])
+weights2 = np.array([.5, -.91, .26, -.5])
+weights3 = np.array([-.26, -.27, .17, .87])
+
+bias1 = 2
+bias2 = 3
+bias3 = .5
+
+output = np.array([
+    np.dot(_inputs, weights1) + bias1,
+    np.dot(_inputs, weights2) + bias2,
+    np.dot(_inputs, weights3) + bias3
+    ]) 
+    
 print(output)
