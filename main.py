db48516d345acc27b4e433f51ee17a6f3d232dee
felix <felixsanchez0124@gmail.com>
2020-05-17 18:09:42 -0500

    adding layers

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-inputs = np.array([1, 2, 3, 2.5])
+inputs = np.array([
+    [1, 2, 3, 2.5],
+    [2, 5, -1, 2],
+    [-1.5, 2.7, 3.3, -.8]
+])
 
 weights = np.array([
     [.2, .8, -.5, 1],
@@ -10,7 +14,15 @@ weights = np.array([
 
 biases = np.array([2, 3, .5])
 
+weights2 = np.array([
+    [0.1, -.14, .5],
+    [-.5, 0.12, -.33],
+    [-.44, .73, -.13]
+])
 
-output = np.dot(weights, inputs) + biases
+biases2 = [-1, 2,-0.5]
 
-print(output)
+layer1_output = np.dot(inputs, weights.T) + biases
+layer2_output = np.dot(layer1_output, weights2.T) + biases2
+
+print(layer2_output)
