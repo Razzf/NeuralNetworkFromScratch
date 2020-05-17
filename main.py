39e46cf9a62d05b4ed15141bd47773e0382cacf9
felix <felixsanchez0124@gmail.com>
2020-05-16 21:27:02 -0500

    code reduced

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import numpy as np
 
-_inputs = np.array([1, 2, 3, 2.5])
+inputs = np.array([1, 2, 3, 2.5])
+
+weights = np.array([
+    [.2, .8, -.5, 1],
+    [.5, -.91, .26, -.5],
+    [-.26, -.27, .17, .87]
+])
+
+biases = np.array([2, 3, .5])
 
 
-weights1 = np.array([.2, .8, -.5, 1])
-weights2 = np.array([.5, -.91, .26, -.5])
-weights3 = np.array([-.26, -.27, .17, .87])
+output = np.dot(weights, inputs) + biases
 
-bias1 = 2
-bias2 = 3
-bias3 = .5
-
-output = np.array([
-    np.dot(_inputs, weights1) + bias1,
-    np.dot(_inputs, weights2) + bias2,
-    np.dot(_inputs, weights3) + bias3
-    ]) 
-    
 print(output)
